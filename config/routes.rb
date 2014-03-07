dce68b3b404017cce71ef73055dcf57dba770ee5
Rahul Parmar <rahul.parmar@gmail.com>
2014-03-06 18:42:09 -0800

    Posts database created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 LighthouseForum::Application.routes.draw do
   get 'posts', to: 'posts#index'
-  get 'posts/0', to: 'posts#post0'
-  get 'posts/1', to: 'posts#post1'
-  get 'posts/2', to: 'posts#post2'
-  get 'posts/3', to: 'posts#post3'
+  get 'posts/:id', to: 'posts#show'
 end
