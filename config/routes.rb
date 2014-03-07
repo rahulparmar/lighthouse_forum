8a452b8674f9b0ae520a03a699c877cf1b54c7ec
Rahul Parmar <rahul.parmar@gmail.com>
2014-03-06 19:27:28 -0800

    Create functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LighthouseForum::Application.routes.draw do
   get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts', to: 'posts#create'
 end
