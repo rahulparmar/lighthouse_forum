b49fa5a46c732e7445a62bf5472ec1f20036150e
Rahul Parmar <rahul.parmar@gmail.com>
2014-03-06 21:05:50 -0800

    Posts made resources, edit/delete functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 LighthouseForum::Application.routes.draw do
-  get 'posts',          to: 'posts#index'
-  get 'posts/new',      to: 'posts#new'
-  get 'posts/:id',      to: 'posts#show', as: 'post'
-  post 'posts',         to: 'posts#create'
-  get 'posts/:id/edit', to: 'posts#edit'
-  patch 'posts/:id',    to: 'posts#update'
+ resources :posts
+ root to: 'posts#index'
 end
